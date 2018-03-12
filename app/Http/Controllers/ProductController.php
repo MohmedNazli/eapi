265a84df5806460dd6cd9eca1df5548943c9ff1c
unknown <mnazli.159@gmail.com>
2018-03-12 13:45:51 +0200

    Update Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -90,7 +90,10 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+
+        $request['detail']=$request->description;
+        unset($request['description']);
+        $product->update($request->all());
     }
 
     /**
